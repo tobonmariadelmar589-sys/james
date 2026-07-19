92f4c7cab0566476557cc7f3ce30ead9a35ddba7
tobonmariadelmar589-sys <tobonmariadelmar589@gmail.com>
2026-07-18 20:27:21 -0500

    Delete terrario1/terrario directory

--- terrario1/terrario/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\LandingController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [LandingController::class, 'index'])->name('landing');
-Route::post('/contacto', [LandingController::class, 'contact'])->name('landing.contact');
